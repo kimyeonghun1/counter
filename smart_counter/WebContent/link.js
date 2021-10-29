3ec40d151301f65d078d3df9d9001241f38f3fe8
smhrd <smhrd@220.93.229.184>
2021-10-29 11:46:42 +0900

    영훈이형꺼 수정

--- smart_counter/WebContent/link.js
+++ smart_counter/WebContent/link.js
@@ -14,27 +14,27 @@ const app = new Vue ({
     postList : [
       new Post(
         '풀업', 
-        '<iframe width="1280" height="720" src="https://www.youtube.com/embed/nWhS28U6bCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', 
-        '바벨라토르 홈트레이닝', 
-        'https://img.youtube.com/vi/nWhS28U6bCY/0.jpg'
+        'https://www.youtube.com/watch?v=nWhS28U6bCY&t=1s', 
+        '바벨라토르', 
+        'https://i.ytimg.com/an_webp/nWhS28U6bCY/mqdefault_6s.webp?du=3000&sqp=CNT67IsG&rs=AOn4CLBdmnu4wFUAhQF2XQXzxZG_QMH9xg'
       ),
       new Post(
         '친업', 
         'https://www.youtube.com/watch?v=6GWGVAO8oU4', 
         '핏블리',
-        'https://img.youtube.com/vi/6GWGVAO8oU4/0.jpg'
+        'https://i.ytimg.com/an_webp/6GWGVAO8oU4/mqdefault_6s.webp?du=3000&sqp=COzP7IsG&rs=AOn4CLAb-DMNPj6ByQ0j6MDn_hqML2NsGA'
       ),
       new Post(
         '풀업VS친업', 
         'https://www.youtube.com/watch?v=1oIi0g363MI', 
         '피지컬갤러리', 
-        'https://img.youtube.com/vi/1oIi0g363MI/0.jpg'
+        'https://i.ytimg.com/an_webp/1oIi0g363MI/mqdefault_6s.webp?du=3000&sqp=CO6t7YsG&rs=AOn4CLDt2M5BLpjgy7JDHmemLAuUVUESjw'
       ),
       new Post(
         '푸쉬업', 
-        'https://www.youtube.com/watch?v=aoH7qNedO8k', 
-        '피지컬갤러리',
-        'https://img.youtube.com/vi/aoH7qNedO8k/0.jpg'
+        'https://www.youtube.com/watch?v=-_DUjHxgmWk&t=1s', 
+        '바벨라토르',
+        'https://i.ytimg.com/an_webp/-_DUjHxgmWk/mqdefault_6s.webp?du=3000&sqp=CPjt7IsG&rs=AOn4CLAjTzaPO41ERnTYRpLeql1SGTwjhQ'
       ),
       new Post(
         '25가지 푸쉬업', 
@@ -46,26 +46,27 @@ const app = new Vue ({
         '싯업', 
         'https://www.youtube.com/watch?v=tfwbpv6gCbg', 
         '지피티',
-        'https://img.youtube.com/vi/tfwbpv6gCbg/0.jpg'
+        'https://i.ytimg.com/an_webp/tfwbpv6gCbg/mqdefault_6s.webp?du=3000&sqp=CNit7YsG&rs=AOn4CLCQKcca5pEf6Ely9sQFFth_WpcV0g'
       ),
       new Post(
         '스쿼트', 
         'https://www.youtube.com/watch?v=QyAH8O5X6g0', 
         '강경원',
-        'https://img.youtube.com/vi/QyAH8O5X6g0/0.jpg'
+        'https://i.ytimg.com/an_webp/QyAH8O5X6g0/mqdefault_6s.webp?du=3000&sqp=CMCy7YsG&rs=AOn4CLCQEAQW5SiPE6gwBtzPRu-eu7a9vQ'
       ),
       new Post(
         '하체스트레칭', 
         'https://www.youtube.com/watch?v=UBVPNC1FBgc', 
         '피지컬갤러리', 
-        'https://img.youtube.com/vi/UBVPNC1FBgc/0.jpg'
+        'https://i.ytimg.com/an_webp/UBVPNC1FBgc/mqdefault_6s.webp?du=3000&sqp=CPiJ7YsG&rs=AOn4CLDigZJtQN95jyvaCuke0E446zhChQ'
       ),
       new Post(
         '상체스트레칭', 
         'https://www.youtube.com/watch?v=FsTkLYaUh_o', 
         '피지컬갤러리',
-        'https://img.youtube.com/vi/FsTkLYaUh_o/0.jpg'
-      ),
+        'https://i.ytimg.com/an_webp/FsTkLYaUh_o/mqdefault_6s.webp?du=3000&sqp=CNau7YsG&rs=AOn4CLAL9ltsF7tfa1c1f0Ht4ihOIJFn7Q'
+      )
+     
 ]
   },
   computed: {
